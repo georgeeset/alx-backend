be223fe4c700a298cbd278dc22a85fa0974cb50a
georgeeset <georgeperfect4u@gmail.com>
2023-10-31 22:57:58 +0100

    pycode style resolved app.config position changed

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,6 +14,8 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app.config.from_object('1-app.Config')
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """
@@ -22,7 +24,5 @@ def index():
     return render_template('0-index.html')
 
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-    app.config.from_object('1-app.Config')
