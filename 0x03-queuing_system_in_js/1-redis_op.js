61a8806d6b50ad69b4b4c4a97ff3b29beb61973c
georgeeset <georgeperfect4u@gmail.com>
2023-11-29 03:04:43 +0100

    store and retrieve key value pair

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,35 @@
+#!/usr/bin/yarn dev
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+/**
+ * Set store key value pair in memory
+ * @param {*} schoolName 
+ * @param {*} value 
+ */
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+/**
+ * print value of a stored key
+ * @param {*} schoolName 
+ */
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
